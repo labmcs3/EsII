478534fc2978b1f5fcee4e484cb81c9c17c5424c
Fabrizio Parodi <fabrizio.parodi@ge.infn.it>
2024-10-02 17:21:33 +0200

    update

--- Main.cpp
+++ Main.cpp
@@ -45,11 +45,12 @@ int main(){
   // STEP 1 - opzione 2
   // * Creazione e riempimento vector<MatPoint>
   // * Creazione di OdeSolver passando vector<MatPoint>
-  while (f >> mass >> vx >> x >> vy >> y >> vz >> z){
+  while (f >> mass >> x >> y >> z >> vx >> vy >> vz){
     //...
   }
 
   // Creazione classe OdeSolver (per la soluzione dell'equ. diff.)
+  double giorno = 86400; // giorno in secondi
   ode.fInternal = fInternal;
   ode.fExternal = fExternal;
   ode.DeltaT(0.5);
@@ -60,7 +61,8 @@ int main(){
   TCanvas c("c","",10,10,500,500);
   
   //Preparazione grafico delle coordinate dei pianeti
-  double size=5; // 5 unita' astronimiche
+  double au = 1.5e8;   // unita' astronimica in km
+  double size = 5*au;  // 5 unita' astronimiche
   gPad->DrawFrame(-size,-size,size,size);
   int color[10]={kOrange+1,kViolet+1,kGreen+2,kAzure+1,kRed+2,kRed-7,kCyan-8,kBlue-7,kBlue+1,kBlue+2};
   for (unsigned int i=0;i<ode.N();i++){
@@ -73,7 +75,7 @@ int main(){
   app.Run(true);
 
   //Run del metodo numerico + grafico in tempo reale delle coordinate e del mom. angolare totale
-  while (ode.T()<365){
+  while (ode.T()<365*giorno){
     ode.Step();
     for (unsigned int i=0;i<ode.N();i++){
       //STEP 4 riempimento delle grafico gr[i] con le coordinate aggiornate dei pianeti
